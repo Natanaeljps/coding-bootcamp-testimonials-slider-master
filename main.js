59541ff446dccc02e453d7b050827e471695b4b3
NatanaelJPS <natanaeljps@gmail.com>
2021-07-09 22:06:20 -0400

    verson2

---
+++ main.js
@@ -0,0 +1,15 @@
+
+const next = document.getElementById("john-btn");
+
+next.addEventListener("click", function () {
+
+  const apareceJohn = document.getElementById("john");
+
+  apareceJohn.classList.toggle("hide");
+
+  
+});
+
+
+
+
